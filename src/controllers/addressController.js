3935e99fc2352810797d97ca8d9be3820d951a1f
ziolata <ziolata3@gmail.com>
2025-06-20 09:32:08 +0700

    feat(address): add update address and delete address function

--- src/controllers/addressController.js
+++ src/controllers/addressController.js
@@ -2,8 +2,8 @@ import * as services from "../services/address_service";
 
 export const createAddressController = async (req, res) => {
 	try {
-		req.body.user_id = req.user.id;
-		const response = await services.createAddress(req.body);
+		const user_id = req.user.id;
+		const response = await services.createAddress(user_id);
 		return res.status(201).json(response);
 	} catch (error) {
 		const status = error.status;
@@ -11,10 +11,37 @@ export const createAddressController = async (req, res) => {
 		return res.status(status).json({ message });
 	}
 };
+
 export const getAllAddressController = async (req, res) => {
 	try {
-		req.body.user_id = req.user.id;
-		const response = await services.getAllAddress(req.body);
+		const user_id = req.user.id;
+		const response = await services.getAllAddress(user_id);
+		return res.status(201).json(response);
+	} catch (error) {
+		const status = error.status;
+		const message = error.message;
+		return res.status(status).json({ message });
+	}
+};
+
+export const updateAddressController = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = req.user;
+		const response = await services.updateAddress(id, user);
+		return res.status(201).json(response);
+	} catch (error) {
+		const status = error.status;
+		const message = error.message;
+		return res.status(status).json({ message });
+	}
+};
+
+export const deleteAddressController = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = req.user;
+		const response = await services.deleteAddress(id, user);
 		return res.status(201).json(response);
 	} catch (error) {
 		const status = error.status;
--- src/routes/address_routes.js
+++ src/routes/address_routes.js
@@ -6,4 +6,7 @@ const routes = new Router();
 
 routes.post("/add", isAuthenticated, controller.createAddressController);
 routes.get("/", isAuthenticated, controller.getAllAddressController);
+routes.get("/update/:id", isAuthenticated, controller.updateAddressController);
+routes.get("/delete/:id", isAuthenticated, controller.deleteAddressController);
+
 export default routes;
