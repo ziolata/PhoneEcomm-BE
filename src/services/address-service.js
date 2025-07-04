bcb88d26c057a94a13397a544e3517eb02de9caf
ziolata <ziolata3@gmail.com>
2025-07-04 10:38:09 +0700

    fix: fix createAddress and getAddressOrThrowById

--- src/services/address-service.js
+++ src/services/address-service.js
@@ -2,7 +2,7 @@ import db from "../models/index.js";
 import { successResponse, throwError } from "../utils/response-utils.js";
 
 export const getAddressOrThrowById = async (id) => {
-	const foundAddress = await db.Address.findOne({ where: id });
+	const foundAddress = await db.Address.findOne({ where: { id } });
 	if (!foundAddress) {
 		throwError(404, `Không tìm thấy địa chỉ có id: ${id}!`);
 	}
@@ -13,17 +13,17 @@ export const createAddress = async (data) => {
 	return successResponse("Thêm địa chỉ thành công!", response);
 };
 
-export const getAllAddressByUserId = async (user) => {
+export const getAllAddressByUserId = async (user_id) => {
 	const response = await db.Address.findAll({
 		where: {
-			user_id: user,
+			user_id,
 		},
 	});
 	if (!response) {
-		throwError(400, `Không tìm thấy địa chỉ của user_id: ${user}`);
+		throwError(400, `Không tìm thấy địa chỉ của user_id: ${user_id}`);
 	}
 	return successResponse(
-		`Lấy danh sách địa chỉ của user_id: ${user} thành công!`,
+		`Lấy danh sách địa chỉ của user_id: ${user_id} thành công!`,
 		response,
 	);
 };
@@ -31,17 +31,21 @@ export const getAllAddressByUserId = async (user) => {
 export const updateAddress = async (id, user, data) => {
 	const foundAddress = await getAddressOrThrowById(id);
 	if (foundAddress.user_id !== user.id && user.role !== "ADMIN") {
-		throwError(400, "Bạn không có quyền cập nhật địa chỉ của tài khoản khác !");
+		throwError(400, "Bạn không có quyền cập nhật địa chỉ của tài khoản khác!");
 	}
-	await db.Address.update(data, { where: id });
+	await db.Address.update(data, { where: { id } });
 	return successResponse("Cập nhật địa chỉ thành công!");
 };
 
 export const deleteAddress = async (id, user) => {
 	const foundAddress = await getAddressOrThrowById(id);
 	if (foundAddress.user_id !== user.id && user.role !== "ADMIN") {
-		throwError(400, "Bạn không có quyền xóa địa chỉ của tài khoản khác !");
+		throwError(400, "Bạn không có quyền xóa địa chỉ của tài khoản khác!");
 	}
-	await db.Address.destroy({ where: id });
+	await db.Address.destroy({
+		where: {
+			id,
+		},
+	});
 	return successResponse("Xóa địa chỉ thành công!");
 };
