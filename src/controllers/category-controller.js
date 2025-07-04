6a8373e1817e8865b7ae6ae669c44c72618e16c4
ziolata <ziolata3@gmail.com>
2025-07-04 10:39:18 +0700

    refactor: move upload image logic from controller to service fix: only upload image when create category or product succeeds

--- src/controllers/category-controller.js
+++ src/controllers/category-controller.js
@@ -1,16 +1,8 @@
 import * as Services from "../services/category-service.js";
-import { uploadImage } from "../utils/cloudinary-utils.js";
 
 export const createCategoryController = async (req, res, next) => {
 	try {
-		const imgFile = req.files.img;
-		const uploadResult = await uploadImage(
-			imgFile.tempFilePath,
-			req.body.name,
-			"Category",
-		);
-		req.body.img = uploadResult.img_url;
-		const response = await Services.addCategory(req.body);
+		const response = await Services.addCategory(req.body, req.files?.img);
 		return res.status(201).json(response);
 	} catch (error) {
 		next(error);
@@ -20,14 +12,11 @@ export const createCategoryController = async (req, res, next) => {
 export const updateCategoryController = async (req, res, next) => {
 	try {
 		const { id } = req.params;
-		const imgFile = req.files.img;
-		const uploadResult = await uploadImage(
-			imgFile.tempFilePath,
-			req.body.name,
-			"Category",
+		const response = await Services.updateCategory(
+			id,
+			req.body,
+			req.files?.img,
 		);
-		req.body.img = uploadResult.img_url;
-		const response = await Services.updateCategory(id, data);
 		return res.status(200).json(response);
 	} catch (error) {
 		next(error);
--- src/services/category-service.js
+++ src/services/category-service.js
@@ -5,6 +5,7 @@ import {
 	categoryValidate,
 	updateCategoryValidate,
 } from "../validations/category-validation.js";
+import { uploadImage } from "../utils/cloudinary-utils.js";
 
 const throwIfCategoryNameExists = async (name) => {
 	const foundCategory = await db.Category.findOne({ where: { name } });
@@ -19,17 +20,44 @@ const getCategoryOrThrowById = async (id) => {
 		throwError(404, "Danh mục không tồn tại");
 	}
 };
-export const addCategory = async (data) => {
+export const addCategory = async (data, imgFile) => {
+	if (imgFile) {
+		data.img = { size: imgFile.size };
+	}
 	const validData = handleValidate(categoryValidate, data);
-	await throwIfCategoryNameExists(categoryValidate.name);
+	await throwIfCategoryNameExists(validData.name);
+
+	const image = await uploadImage(
+		imgFile.tempFilePath,
+		validData.name,
+		"Categories",
+	);
+	validData.img = image;
+
 	const response = await db.Category.create(validData);
 	return successResponse("Thêm thành công!", response);
 };
 
-export const updateCategory = async (id, data) => {
-	const validData = handleValidate(updateCategoryValidate, data);
-	await throwIfCategoryNameExists(validData.name);
+export const updateCategory = async (id, data, imgFile) => {
 	await getCategoryOrThrowById(id);
+	// Gắn giá trị cho img nếu có file gửi lên
+	if (imgFile) {
+		data.img = { size: imgFile.size };
+	}
+	const validData = handleValidate(updateCategoryValidate, data);
+	// Kiêm tra tồn tại nếu có trường name cập nhật
+	if (validData.name) {
+		await throwIfCategoryNameExists(validData.name);
+	}
+	// Giá trị img tồn tại thì bắt đầu upload và lấy url upload gắn vào valid.img
+	if (validData.img) {
+		const image = await uploadImage(
+			imgFile.tempFilePath,
+			validData.name,
+			"Categories",
+		);
+		validData.img = image;
+	}
 	await db.Category.update(validData, {
 		where: {
 			id,
@@ -49,18 +77,11 @@ export const deleteCategory = async (id) => {
 };
 
 export const getAllCategory = async () => {
-	const response = await db.Category.findAll({
-		attributes: ["id", "name", "img", "description"],
-	});
+	const response = await db.Category.findAll();
 	return successResponse("Lấy danh sách danh mục thành công!", response);
 };
 
 export const getOneCategory = async (id) => {
-	const response = await db.Category.findByPk(id, {
-		attributes: ["id", "name", "img", "description"],
-	});
-	return successResponse(
-		`Lấy thông tin danh mục có id: ${id} thành công!`,
-		response,
-	);
+	const response = await db.Category.findByPk(id);
+	return successResponse("Lấy thông tin danh mục thành công!", response);
 };
